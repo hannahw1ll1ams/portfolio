7cee33f64d122b1735be3fd906a0990e12576d80
Hannah Williams <hannahw1ll1ams@yahoo.com>
2023-11-15 16:27:20 +0000

    header - adding inter fonts and website images, creating header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header';
+
+//fonts
+import './fonts/Inter-Black.woff'
+import './fonts/Inter-Bold.woff'
+import './fonts/Inter-italic.ttf'
+import './fonts/Inter-Medium.woff2'
+import './fonts/Inter-Regular.woff'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Header/>
       </header>
     </div>
   );
