8052f7c04dd0128282fb6d98b34ac113f074ee78
Hannah Williams <hannahw1ll1ams@yahoo.com>
2023-11-21 17:28:31 +0000

    added illustration images, replaced favicon

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -19,10 +19,7 @@ export default function AboutPage() {
                         <h2 className="bio">I am a developer with a passion for design. I combine these disciplines to deliver visually appealing, intuitive and high-quality digital experiences. </h2>
                         <p className="description">I approach both design and coding in the same way, able to adapt my approaches and processes across projects and languages. Combining my knowledge of what the technology is capable of with the design goals, I can maximise the potential of outcomes closing the disconnect that can exist between the two disciplines.</p>
                     </div>
-                    
                 </div>
-                   
-                
             </div>
             <TechnologiesComponent />
         </div>
