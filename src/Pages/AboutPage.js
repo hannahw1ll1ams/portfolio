8defc1cb0aadd759c982d9d1bced8d21849223b1
Hannah Williams <hannahw1ll1ams@yahoo.com>
2023-11-21 10:22:11 +0000

    fix url redirect for linnked in

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -16,7 +16,7 @@ export default function AboutPage() {
                 <div className="aboutMe-information">
                     <img className="profile-image" src= {profileImage} alt='procprofileImagereatswifteIcon'/>
                     <div className="info">
-                        <h2 className="bio">I am a developer with a passion for design. i combine these disciplines to deliver visually appealing, intuitive and high-quality digital experiences. </h2>
+                        <h2 className="bio">I am a developer with a passion for design. I combine these disciplines to deliver visually appealing, intuitive and high-quality digital experiences. </h2>
                         <p className="description">I approach both design and coding in the same way, able to adapt my approaches and processes across projects and languages. Combining my knowledge of what the technology is capable of with the design goals, I can maximise the potential of outcomes closing the disconnect that can exist between the two disciplines.</p>
                     </div>
                     
