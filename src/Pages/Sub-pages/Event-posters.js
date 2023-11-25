e95a0bb395b8931ecb2e7112980d460613bbc60b
Hannah Williams <hannahw1ll1ams@yahoo.com>
2023-11-25 20:27:22 +0000

    added event posters page

--- src/Pages/Sub-pages/Event-posters.js
+++ src/Pages/Sub-pages/Event-posters.js
@@ -1,7 +1,38 @@
 import React from "react";
+import '../../css/Sub-pages/event-posters.css'
+
+import anniversaryBall from '../../images/rowing/anniversaryBall.png'
+import springBall from '../../images/rowing/springBall.png'
+
+
 
 export default function EventPostersPage() {
     return (
-       <p>Event Posters Page</p>
+        <>
+        <div className="anniversaryBallContainer">
+                <div className="topText">
+                    <h1>EVENT POSTERS</h1>
+                    <h1>2022</h1>
+                </div>
+                <img className="singleImageWidth" src= {springBall} alt='anniversaryBall'/>
+                <div className="bottomText">
+                    <h1></h1>
+                    <p className="imageKeyWords">consistent branding</p>
+                </div>
+        </div>
+
+        <div className="springBallContainer">
+                <div className="topText">
+                    <h1></h1>
+                    <h1>2023</h1>
+                </div>
+                <img className="singleImageWidth" src= {anniversaryBall} alt='springBall'/>
+                <div className="bottomText">
+                    <h1></h1>
+                    <p className="imageKeyWords"></p>
+                </div>
+        </div>
+
+</>
     );
 }
