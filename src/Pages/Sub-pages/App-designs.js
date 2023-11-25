549df8ba8b9b3220b3d7b7f0b09249134655b193
Hannah Williams <hannahw1ll1ams@yahoo.com>
2023-11-25 22:20:36 +0000

    adding base images to portfolio pages

--- src/Pages/Sub-pages/App-designs.js
+++ src/Pages/Sub-pages/App-designs.js
@@ -1,7 +1,25 @@
 import React from "react";
+import '../../css/Sub-pages/app-design.css'
+import requestsLogo from '../../images/appDesign/requestsAppLogo.png'
+import selfServeLogo from '../../images/appDesign/selfServeLogo.png'
 
 export default function AppDesignsPage() {
     return (
-       <p>App Design Page</p>
+        <>
+        <div className="appDesignContainer">
+                <div className="topText">
+                    <h1>APP DESIGN</h1>
+                    <h1>2023</h1>
+                </div>
+                <div className="logoImageContainer">
+                    <img className="appLogoDesign" src= {requestsLogo} alt='requestsLogo'/>
+                    <img className="appLogoDesign" src= {selfServeLogo} alt='selfServeLogo'/>
+                </div>
+                <div className="bottomText">
+                    <h1>PROCESS</h1>
+                    <p className="imageKeyWords">vector drawing / app logo design</p>
+                </div>
+        </div>
+        </>
     );
 }
--- src/Pages/Sub-pages/Strategic-Plan.js
+++ src/Pages/Sub-pages/Strategic-Plan.js
@@ -1,7 +1,61 @@
 import React from "react";
+import finalVersion from '../../images/rowing/strategicPlan3.PNG'
+import version1 from '../../images/rowing/strategicPlan1.jpeg'
+import version2 from '../../images/rowing/strategicPlan2.png'
+import planPillars from '../../images/rowing/planPillars.png'
+
+import '../../css/Sub-pages/strategic-plan.css'
 
 export default function StrategicPlanPage() {
     return (
-       <p>Strategic Plan Page</p>
+       <>
+        <div className="finalVersionContainer">
+                <div className="topText">
+                    <h1></h1>
+                    <h1>2022</h1>
+                </div>
+                <img className="portraitWidth" src= {finalVersion} alt='finalVersion'/>
+                <div className="bottomText">
+                    <h1></h1>
+                    <p className="imageKeyWords">iterations / collaboration / communication / styles / procreate / vectors</p>
+                </div>
+        </div>
+
+       <div className="version1Container">
+                <div className="topText">
+                    <h1></h1>
+                    <h1>2022</h1>
+                </div>
+                <img className="portraitWidth" src= {version1} alt='version1'/>
+                <div className="bottomText">
+                    <h1></h1>
+                    <p className="imageKeyWords">consistent branding</p>
+                </div>
+        </div>
+
+        <div className="version2Container">
+                <div className="topText">
+                    <h1></h1>
+                    <h1>2022</h1>
+                </div>
+                <img className="portraitWidth" src= {version2} alt='version2'/>
+                <div className="bottomText">
+                    <h1></h1>
+                    <p className="imageKeyWords">consistent branding</p>
+                </div>
+        </div>
+
+        <div className="planPilarsContainer">
+                <div className="topText">
+                    <h1>STRATEGIC PLAN PILLARS</h1>
+                    <h1>2022</h1>
+                </div>
+                <img className="landscapeWidth" src= {planPillars} alt='version1'/>
+                <div className="bottomText">
+                    <h1></h1>
+                    <p className="imageKeyWords">consistent branding</p>
+                </div>
+        </div>
+       </>
     );
 }
