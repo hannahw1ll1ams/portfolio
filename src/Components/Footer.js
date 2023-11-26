5264d9d83d9807a6544658a68c25c61d706984ed
Hannah Williams <hannahw1ll1ams@yahoo.com>
2023-11-26 23:24:04 +0000

    clean up redundant code, add footer to all portfolio pages, update contact page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../css/footer.css';
 
-import logo from '../images/logo.PNG'
-
 export default function Footer () {
 
 	return ( 
-		<div class="footer background"> 
-		<a href='/contact'><p className="text" >LET'S CONNECT</p></a>
-		</div> 
+		<footer class="footer background"> 
+			<a href='/contact'><p className="text" >LET'S CONNECT</p></a>
+		</footer> 
 	) 
 } 
